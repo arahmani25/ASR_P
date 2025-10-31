5b542978cd00beda8d120963183e49b021728ba8
arahmani25 <shahrahmani1998@gmail.com>
2025-11-01 01:24:36 +0300

    initial commit ASR_P

--- src/vite.config.ts
+++ src/vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/ASR_Portfolio/', // GitHub Pages base path for repository
+  base: '/ASR_P/', // GitHub Pages base path for repository
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './'),
